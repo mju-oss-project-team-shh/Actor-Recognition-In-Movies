0fdfc39d8cd76bc5af052b4ab331e4f16f686f64
noah0316 <noa316@naver.com>
2020-11-28 21:21:43 +0900

    feat: Search and get image with google

---
+++ getActorImage.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+from urllib.request import urlretrieve
+from selenium import webdriver
+import os
+import ssl
+import asyncio
+
+# Search and get image with google
+
+
+def getActorImage(search):
+
+    ssl._create_default_https_context = ssl._create_unverified_context
+    url = f'https://www.google.com/search?q={quote_plus(search)}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjU-KPJ1YjrAhWbZt4KHRvQDPUQ_AUoAXoECA0QAw&biw=1920&bih=925'
+
+    path = "your chrome driver path"
+    driver = webdriver.Chrome(path)
+    driver.get(url)
+    for i in range(500):
+        driver.execute_script('window.scrollBy(0, 10000)')
+
+    html = driver.page_source
+    soup = BeautifulSoup(html)
+    img = soup.select('.rg_i.Q4LuWd')
+
+    n = 1
+    imgurl = []
+
+    for i in img:
+        try:
+            imgurl.append(i.attrs["src"])
+        except KeyError:
+            imgurl.append(i.attrs["data-src"])
+
+    for i in imgurl:
+        path = f'./dataset/actors/{search}/'
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        filename = '0000000' + str(n)
+        print("파일이름" + filename)
+        if(n > 9):
+            filename = '000000' + str(n)
+        urlretrieve(i, path + filename + ".jpg")
+        n += 1
+        print(str(n) + "번째 사진 저장 완료!")
+
+        # 이미지 파일 갯수 70개 제한
+        if(n == 70):
+            break
+
+        print(url)
+
+    driver.close()
--- run.py
+++ run.py
@@ -8,11 +8,13 @@ from consolemenu.screen import Screen
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from getActorList import searchByTitle
+from getActorImage import getActorImage
 
 menu = None
 movieName = None
 chosenFile = None
 fileType = None
+
 
 def renderPrologueByMovieName():
     global movieName
@@ -22,6 +24,7 @@ def renderPrologueByMovieName():
         mn = movieName
     return "Movie name: " + mn
 
+
 def renderPrologueByFilePath():
     global chosenFile
     if chosenFile is None:
@@ -29,6 +32,7 @@ def renderPrologueByFilePath():
     else:
         cf = chosenFile
     return "Chosen file: " + cf
+
 
 def chooseFileFromFolder(folderPath):
     global fileType
@@ -52,17 +56,19 @@ def chooseFileFromFolder(folderPath):
     else:
         return None
     filename = askopenfilename(
-        initialdir = folderPath,
-        title = title,
-        filetypes = filetypes
+        initialdir=folderPath,
+        title=title,
+        filetypes=filetypes
     )
     return filename
+
 
 def enterMovieName():
     global menu, movieName
     movieName = input("Enter movie name: ")
     menu.prologue_text = renderPrologueByMovieName()
     menu.draw()
+
 
 def selectFile(folderPath, fileTypeFlag):
     global menu, fileType, chosenFile
@@ -72,48 +78,60 @@ def selectFile(folderPath, fileTypeFlag):
         menu.prologue_text = renderPrologueByFilePath()
         menu.draw()
 
+
 def printMovieName():
     global menu
     menu.prologue_text = renderPrologueByMovieName()
     menu.draw()
+
 
 def printChosenFile():
     global menu
     menu.prologue_text = renderPrologueByFilePath()
     menu.draw()
 
+
 def startCommand():
     global menu, fileType, chosenFile, movieName
     # TODO: Start commands
     # [1] Get actors's name
-    searchByTitle(movieName)
+    actorList = []
+    actorList = searchByTitle(movieName)
     # [2] Crawl
+    if actorList:
+        for i in actorList:
+            getActorImage(i)
+    else:
+        print("배우 리스트가 비어있습니다.")
     #
     print("All process completed")
     for i in range(5):
         print("Return to menu at " + str(5 - i) + " second(s)")
         sleep(1)
 
+
 def main():
     global menu
     menu = ConsoleMenu(
-        "< Actor Recognition In Movies v2 >", 
+        "< Actor Recognition In Movies v2 >",
         "Choose the file type & select the file",
-        prologue_text = renderPrologueByMovieName(),
-        epilogue_text = "Made by divya2raj & modified by noah0316, jidungg, 13circle.",
-        formatter = MenuFormatBuilder()
-            .set_title_align("center")
-            .set_subtitle_align("center")
-            .set_border_style_type(MenuBorderStyleType.DOUBLE_LINE_BORDER)
-            .show_prologue_top_border(True)
-            .show_prologue_bottom_border(True)
+        prologue_text=renderPrologueByMovieName(),
+        epilogue_text="Made by divya2raj & modified by noah0316, jidungg, 13circle.",
+        formatter=MenuFormatBuilder()
+        .set_title_align("center")
+        .set_subtitle_align("center")
+        .set_border_style_type(MenuBorderStyleType.DOUBLE_LINE_BORDER)
+        .show_prologue_top_border(True)
+        .show_prologue_bottom_border(True)
     )
 
-    input_movie_name_item = FunctionItem("Enter movie name", enterMovieName, [])
+    input_movie_name_item = FunctionItem(
+        "Enter movie name", enterMovieName, [])
     sel_img_item = FunctionItem("Select image", selectFile, [".", "image"])
     sel_vid_item = FunctionItem("Select video", selectFile, [".", "video"])
-    get_movie_name_item = FunctionItem("Show movie name", printMovieName, []);
-    get_chosen_file_item = FunctionItem("Show chosen file", printChosenFile, []);
+    get_movie_name_item = FunctionItem("Show movie name", printMovieName, [])
+    get_chosen_file_item = FunctionItem(
+        "Show chosen file", printChosenFile, [])
     input_item = FunctionItem("Start Conversion", startCommand, [])
 
     menu.append_item(input_movie_name_item)
@@ -125,6 +143,6 @@ def main():
 
     menu.show()
 
+
 if __name__ == "__main__":
     main()
-
