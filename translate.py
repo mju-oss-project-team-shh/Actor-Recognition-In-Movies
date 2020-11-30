fd679666b4e590d42c08e3715c7e445516505983
13circle <13circle97@gmail.com>
2020-11-30 16:22:21 +0900

    Split space-separated name & partially romanize

--- translate.py
+++ translate.py
@@ -1,28 +1,39 @@
 import json
-import urllib.request
+import urllib
+from urllib import parse
+from urllib.request import Request, urlopen
 import ssl
+from bs4 import BeautifulSoup
 
-client_id = "Client_id"
-client_secret = "Client_secret"
+baseUrl = "https://openapi.naver.com/v1/krdict/romanization?query="
+client_id = "2SULrTwEBG_Y1J6zzlKw"
+client_secret = "PNxbkrEUFt"
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def translateKRToEN(name):
-    encText = urllib.parse.quote(name)
-    url = "https://openapi.naver.com/v1/krdict/romanization?query=" + encText
+    spNames = name.split(" ")
+    tNames = []
+    for n in spNames:
+        encText = parse.quote(n)
+        url = baseUrl + encText
 
-    request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id", client_id)
-    request.add_header("X-Naver-Client-Secret", client_secret)
-    response = urllib.request.urlopen(request)
-    rescode = response.getcode()
+        request = urllib.request.Request(url)
+        request.add_header("X-Naver-Client-Id", client_id)
+        request.add_header("X-Naver-Client-Secret", client_secret)
+        response = urllib.request.urlopen(request)
+        rescode = response.getcode()
 
-    if(rescode == 200):
-        response_body = response.read()
-        json_dict = json.loads(response_body.decode('utf-8'))
-        result = json_dict['aResult'][0]
-        name_items = result['aItems']
-        names = [name_item['name'] for name_item in name_items]
-        return names[0].replace(" ", "_")
-    else:
-        print("Error Code:" + rescode)
+        if(rescode == 200):
+            response_body = response.read()
+            json_dict = json.loads(response_body.decode('utf-8'))
+            print(json_dict)
+            result = json_dict['aResult'][0]
+            name_items = result['aItems']
+            names = [name_item['name'] for name_item in name_items]
+            tNames.append(names[0].replace(" ", "_"))
+        else:
+            print("Error Code:" + rescode)
+    return ' '.join(tNames)
+
+
